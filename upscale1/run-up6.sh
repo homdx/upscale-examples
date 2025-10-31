e0227adcbcf4e83935a4b50a69ed8f4ba98055af
homdx <37062532+homdx@users.noreply.github.com>
2025-10-31 10:19:30 +0300

    Update to upscale version 2024 New Year

--- upscale1/run-up6.sh
+++ upscale1/run-up6.sh
@@ -17,14 +17,16 @@ echo Removing leading zeroes from a variable $start1 is now is $start
 #exit
 #start=318
 echo will be start from $start file name
-increment=6
 increment=1
 end=4899
 counter=1
 upscale="/home/user/Project/squashfs-root/resources"
 # Windows: upscale="/c/upscale20024/resources"
+modelmode="ultrasharp-4x"
+# ultramix-balanced-4x
 #Extract AppImage with command line: --appimage-extract
 sourcesfolder="/mnt/srcsfolder"
+# ultramix-balanced-4x
 resultsfolder="/mnt/resultsfolder"
 # Windows: sourcesfolder="c:\\srcfolder"
 # Windows: resultsfolder="c:\\resultsfolder"
@@ -42,22 +44,23 @@ for i in $(seq -f "%04g" $start $increment $end); do
 #    echo $i
     f=thumb$i.png
     echo $f
-#    runcmd="$upscale/bin/upscayl-bin  -i $sourcesfolder/$f -o $resultsfolder/$f  -s 4 -m $upscale/models -n realesrgan-x4plus  -f png -g 0"
-#  Now is supporting the 2.15 version
-    runcmd="$upscale/bin/upscayl-bin  -i $sourcesfolder/$f -o $resultsfolder/$f  -s 4 -m $upscale/models -n high-fidelity-4x  -f png -g 0"
-    # Windows: runcmd="$upscale/bin/upscayl-bin.exe  -i $sourcesfolder\\$f -o $resultsfolder\\$f  -s 4 -m $upscale\\models -n high-fidelity-4x  -f png -g 0"
+    runcmd="$upscale/bin/upscayl-bin  -i $f -o $resultsfolder/$f -m $upscale/models -n $modelmode -f png -g 0"
+    # Windows: runcmd="$upscale/bin/upscayl-bin.exe  -i $sourcesfolder\\$f -o $resultsfolder\\$f -m $upscale\\models -n $modelmode -f png -g 0"
     #echo $runcmd >$numfile-tmp.txt
     #runcmd="go run test6.go --num $numfile"
     time $runcmd
+    echo $runcmd >6-tmp.txt
     if [ $? -eq 0 ]; then
     numsave=$(echo $i | sed 's/^0*//')
     echo $numsave >$num
-    echo sequesfull done $i / $end and saved $numsave from $start
+    echo sequesfull done $i and saved $numsave from $start
     date
     else
         exit 1
     fi
+
     echo $runcmd
+    date
     if ((counter % 6 == 0)); then
         start=$((start + increment))
     fi
